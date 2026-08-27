fc943bdb5d5bd82f21859ad7fc8506d6f56bc661
HariHere1 <hg6674763@gmail.com>
2026-08-27 20:18:13 +0530

    Change button color

--- src/App.tsx
+++ src/App.tsx
@@ -1290,7 +1290,7 @@ export default function App() {
           <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
             <path
               d="M10 4v12M4 10h12"
-              stroke="white"
+              stroke="black"
               strokeWidth="2"
               strokeLinecap="round"
             />
